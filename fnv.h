25589c4306baa79b8ac3be6a7c2e4427c08439b9
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-07 18:01:32 +0100

    Move asterisk

--- fnv.h
+++ fnv.h
@@ -1,7 +1,7 @@
 #ifndef FNV_HEADER_FILE
 #define FNV_HEADER_FILE
 
-unsigned int fnv1a(const void* data, register unsigned int numBytes);
+unsigned int fnv1a(const void *data, register unsigned int numBytes);
 unsigned int fnv1r(const void *data, register unsigned int numBytes);
 
 #include "fnv.c"
