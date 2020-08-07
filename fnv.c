0663c369fdb7f42ee33e0779a6a66c3676e1dffa
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-07 23:10:35 +0100

    Ignore data state in fnv.c

--- fnv.c
+++ fnv.c
@@ -5,11 +5,8 @@ unsigned int fnv1a(const void *data, register unsigned int numBytes)
     register unsigned int hash = 0x811C9DC5;
     const unsigned char *ptr = (const unsigned char *)data;
     
-    if (data)
-    {
-        while (numBytes--)
-            hash = (*ptr++ ^ hash) * 0x01000193;
-    }
+    while (numBytes--)
+        hash = (*ptr++ ^ hash) * 0x01000193;
 
     return hash;
 }
@@ -21,11 +18,8 @@ unsigned long long fnv1a64(
     register unsigned long long hash = 0xCBF29CE484222325;
     const unsigned char *ptr = (const unsigned char *)data;
     
-    if (data)
-    {
-        while (numBytes--)
-            hash = (*ptr++ ^ hash) * 0x00000100000001B3;
-    }
+    while (numBytes--)
+        hash = (*ptr++ ^ hash) * 0x00000100000001B3;
         
     return hash;
 }
