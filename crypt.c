814e6b9a25fcb51da1a9c6a382810f0be6558c32
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-07 23:31:02 +0100

    Correct stability issue

--- crypt.c
+++ crypt.c
@@ -5,15 +5,13 @@
 #define CR_KEY_ITERATIONS 16384
 #endif
 
-#define KEY_WIDTH sizeof(uint64_t)
-
 uint64_t cr_keygen(
     const void *key,
     uint32_t numBytesKey,
     uint32_t iterations)
 {
     uint64_t pivot = fnv1a64(key, numBytesKey);
-    while (iterations--) pivot = fnv1a64(&pivot, KEY_WIDTH);
+    while (iterations--) pivot = fnv1a64(&pivot, sizeof(uint64_t));
     return pivot;
 }
 
@@ -23,16 +21,18 @@ void cr_encrypt_decrypt(
     const void *key,
     uint32_t numBytesKey)
 {
-    uint32_t original_length = numBytesDest;
     uint8_t *dcstream = dest;
+
+    uint32_t original_length = numBytesDest;
+
     uint64_t hkey = cr_keygen(key, numBytesKey, CR_KEY_ITERATIONS);
     uint64_t fkey = 0;
 
     while (numBytesDest--)
     {
-        if (!(((original_length - numBytesDest) - 1) % KEY_WIDTH))
+        if (!(((original_length - numBytesDest) - 1) % sizeof(uint64_t)))
         {
-            fkey = fr_rand64(hkey ^ fnv1a64(&numBytesDest, KEY_WIDTH));
+            fkey = fr_rand64(hkey ^ fnv1a64(&numBytesDest, sizeof(uint32_t)));
         } else {
             fkey >>= 1;
         }
