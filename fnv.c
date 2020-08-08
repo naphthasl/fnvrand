d5f39de0906e5aebf3e52e3d049298b1c218f440
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-08 13:43:28 +0100

    Added fnv1a128

--- fnv.c
+++ fnv.c
@@ -7,11 +7,24 @@ uint32_t fnv1a(const void *data, uint32_t numBytes)
     return hash;
 }
 
-uint64_t fnv1a64(const void *data, uint64_t numBytes)
+uint64_t fnv1a64(const void *data, uint32_t numBytes)
 {
     uint64_t hash = 0xCBF29CE484222325;
     const uint8_t *ptr = (const uint8_t *)data;
     
     while (numBytes--) hash = (*ptr++ ^ hash) * 0x00000100000001B3; 
+    return hash;
+}
+
+__uint128_t fnv1a128(const void *data, uint32_t numBytes)
+{
+    const uint64_t hashsplit[] = {0x62B821756295C58D, 0x6C62272E07BB0142};
+    const uint64_t prmesplit[] = {0x000000000000013B, 0x0000000001000000};
+    __uint128_t hash; memcpy(&hash, hashsplit, 16);
+    __uint128_t prme; memcpy(&prme, prmesplit, 16);
+    
+    const uint8_t *ptr = (const uint8_t *)data;
+    
+    while (numBytes--) hash = (*ptr++ ^ hash) * prme; 
     return hash;
 }
--- fnv.h
+++ fnv.h
@@ -3,7 +3,8 @@
 #ifndef FNV_HEADER_FILE
 #define FNV_HEADER_FILE
     uint32_t fnv1a(const void *data, uint32_t numBytes);
-    uint64_t fnv1a64(const void *data, uint64_t numBytes);
+    uint64_t fnv1a64(const void *data, uint32_t numBytes);
+    __uint128_t fnv1a128(const void *data, uint32_t numBytes);
 
     #include "fnv.c"
 #endif
