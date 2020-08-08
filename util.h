4c0ac5fca223d7a4b79a2ae85d9fc55417bf1a8b
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-08 09:46:40 +0100

    Moved bool definition to util.h

--- util.h
+++ util.h
@@ -2,6 +2,10 @@
 
 #ifndef FUTIL_HEADER_FILE
 #define FUTIL_HEADER_FILE
+    #define true 1
+    #define false 0
+    #define bool char
+
     void u_memtohex(void *dest, const void *src, register uint64_t numSrcBytes);
     void u_printmemhex(const void *src, register uint64_t numSrcBytes);
 
