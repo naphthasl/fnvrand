ee422785aa628afe4959d25982665258acec5d8e
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-09 09:28:07 +0100

    Save indent space by continuing past return if it is not called

--- src/fastrand.c
+++ src/fastrand.c
@@ -96,16 +96,16 @@ void RadixAbstract_RandomizeBlob(RadixMemoryBlob *blob, uint64_t seed)
 int fr_uniquerandom(int n) {
     if ((n - 1) == RAND_MAX) {
         return rand();
-    } else {
-        assert (n <= RAND_MAX);
-
-        int end = RAND_MAX / n;
-        assert (end > 0);
-        end *= n;
-
-        int r;
-        while ((r = rand()) >= end);
-
-        return r % n;
     }
+    
+    assert (n <= RAND_MAX);
+
+    int end = RAND_MAX / n;
+    assert (end > 0);
+    end *= n;
+
+    int r;
+    while ((r = rand()) >= end);
+
+    return r % n;
 }
