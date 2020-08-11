25dc32767e1eb1f438477e1b851917deb50fede0
hhhhhhhhhhhhhhh <69372178+marbles-box@users.noreply.github.com>
2020-08-10 21:28:03 -0400

    h
    header file

---
+++ src/fracts.h
@@ -0,0 +1,21 @@
+#ifndef FRACT_HEADER_FILE
+#define FRACT_HEADER_FILE
+	typedef struct fract fract;
+	
+	//fract-fract operations
+	void fract_add_fract(fract *output_fract, fract term_fract);
+	void fract_sub_fract(fract *output_fract, fract term_fract);
+	void fract_mul_fract(fract *output_fract, fract term_fract);
+	void fract_div_fract(fract *output_fract, fract term_fract);
+	
+	//fract unary operations
+	void fract_reciprocate(fract *output_fract);
+	
+	//fract-int operations
+	void fract_add_int(fract *output_fract, int term_int);
+	void fract_sub_int(fract *output_fract, int term_int);
+	void fract_mul_int(fract *output_fract, int term_int);
+	void fract_div_int(fract *output_fract, int term_int);
+	
+	#include "fracts.c"
+#endif
