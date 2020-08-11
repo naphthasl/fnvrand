372b9b11fedae7e02336899880408575c3fd6911
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-11 01:49:32 +0100

    Bool definition safety

--- src/bool.h
+++ src/bool.h
@@ -1,8 +1,11 @@
 #ifndef FBOOL_HEADER_FILE
 #define FBOOL_HEADER_FILE
-    #define true 1
-    #define false 0
-    #define bool char
+    // Just in case it has been defined elsewhere
+    #if !(defined(true) || defined(false) || defined(bool))
+        #define true 1
+        #define false 0
+        #define bool char
+    #endif
 
     #define BOOLIFY(a) ((a)?(true):(false))
 #endif
