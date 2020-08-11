a0b8838828de797833a6e602530ca4e905d051f0
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-11 11:34:57 +0100

    Add R_line3d and R_square3d

--- src/space.h
+++ src/space.h
@@ -8,9 +8,19 @@
         float z;
     } R_point3d;
 
+    typedef struct R_line3d {
+        R_point3d a;
+        R_point3d b;
+    } R_line3d;
+
     typedef struct R_tris3d {
         R_point3d a;
         R_point3d b;
         R_point3d c;
     } R_tris3d;
+
+    typedef struct R_square3d {
+        R_tris3d a;
+        R_tris3d b;
+    } R_square3d;
 #endif
