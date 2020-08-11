c4e2c26709f8c6cb6dc80210dc07ae8e4db4496b
Myself <numzyx@gmail.com>
2020-08-10 21:50:44 -0400

    ->

--- src/fracts.c
+++ src/fracts.c
@@ -9,28 +9,28 @@ typedef struct fract {
 
 //add term fract to output fract
 void fract_add_fract(fract *output_fract, fract term_fract) {
-	*output_fract.num = (*output_fract.num) * term_fract.den +
-		(*output_fract.den) * term_fract.num;
-	*output_fract.den = (*output_fract.den) * term_fract.den;
+	output_fract->num = output_fract->num * term_fract.den +
+		output_fract->den * term_fract.num;
+	output_fract->den = output_fract->den * term_fract.den;
 };
 
 //subtract term fract from output fract
 void fract_sub_fract(fract *output_fract, fract term_fract) {
-	*output_fract.num = (*output_fract.num) * term_fract.den -
-		(*output_fract.den) * term_fract.num;
-	*output_fract.den = (*output_fract.den) * term_fract.den;
+	output_fract->num = output_fract->num * term_fract.den -
+		output_fract->den * term_fract.num;
+	output_fract->den = output_fract->den * term_fract.den;
 };
 
 //multiply output fract by term fract
 void fract_mul_fract(fract *output_fract, fract term_fract) {
-	*output_fract.num = (*output_fract.num) * term_fract.num;
-	*output_fract.den = (*output_fract.den) * term_fract.den;
+	output_fract->num = output_fract->num * term_fract.num;
+	output_fract->den = output_fract->den * term_fract.den;
 };
 
 //divide output fract by term fract
 void fract_div_fract(fract *output_fract, fract term_fract) {
-	*output_fract.num = (*output_fract.num) * term_fract.den;
-	*output_fract.den = (*output_fract.den) * term_fract.num;
+	output_fract->num = output_fract->num * term_fract.den;
+	output_fract->den = output_fract->den * term_fract.num;
 };
 
 /*
@@ -39,9 +39,9 @@ void fract_div_fract(fract *output_fract, fract term_fract) {
 
 //set output fract to its reciprocal
 void fract_reciprocate(fract *output_fract) {
-	int temp = *output_fract.num;
-	*output_fract.num = *output_fract.den;
-	*output_fract.den = temp;
+	int temp = output_fract->num;
+	output_fract->num = output_fract->den;
+	output_fract->den = temp;
 };
 
 /*
@@ -50,20 +50,20 @@ void fract_reciprocate(fract *output_fract) {
 
 //add term int to output fract
 void fract_add_int(fract *output_fract, int term_int) {
-	*output_fract.num = (*output_fract.num) + (*output_fract.den) * term_int;
+	output_fract->num = output_fract->num + output_fract->den * term_int;
 };
 
 //subtract term int from output fract
 void fract_sub_int(fract *output_fract, int term_int) {
-	*output_fract.num = (*output_fract.num) - (*output_fract.den) * term_int;
+	output_fract->num = output_fract->num - output_fract->den * term_int;
 };
 
 //multiply output fract by term int
 void fract_mul_int(fract *output_fract, int term_int) {
-	*output_fract.num = (*output_fract.num) * term_int;
+	output_fract->num = output_fract->num * term_int;
 };
 
 //divide output fract by term int
 void fract_div_int(fract *output_fract, int term_int) {
-	*output_fract.den = (*output_fract.den) * term_int;
+	output_fract->den = output_fract->den * term_int;
 };
